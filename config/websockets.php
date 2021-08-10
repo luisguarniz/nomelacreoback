bf6b5732e552660bc058abb150a81a7e06fa98bb
DunkaN <lguarnizl@gmail.com>
2021-08-10 12:26:04 -0500

    agrege ruta del certifido que se instalo con certbot. cambio realizado en websocket.php

--- config/websockets.php
+++ config/websockets.php
@@ -116,14 +116,16 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
-      //'local_cert' => '/etc/apache2/ssl/nomelacreo.com/server.crt',
+       // 'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null), //de fabrica
+       //'local_cert' => '/etc/apache2/ssl/nomelacreo.com/server.crt', //probando cert que me dio gustavo
+      'local_cert' => '/etc/letsencrypt/live/api.agilefacilitationnetwork.com/cert.pem',//certificado encontrado
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
-       //'local_cert' => '/etc/apache2/ssl/nomelacreo.com/server.key',
+     // 'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null), //de fabrica
+       //'local_cert' => '/etc/apache2/ssl/nomelacreo.com/server.key', //probando cert que me dio gustavo
+       'local_cert' => '/etc/letsencrypt/live/api.agilefacilitationnetwork.com/privkey.pem',//certificado encontrado
 
         /*
          * Passphrase for your local_cert file.
