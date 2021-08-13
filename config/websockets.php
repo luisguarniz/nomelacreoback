e6895b4cd00a553aa2aa01b275e84649b40637ea
DunkaN <lguarnizl@gmail.com>
2021-08-13 11:20:38 -0500

    probando certificados pem

--- config/websockets.php
+++ config/websockets.php
@@ -116,17 +116,17 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null), //de fabrica
+      //  'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null), //de fabrica
        //'local_cert' => '/etc/apache2/ssl/nomelacreo.com/server.crt', //probando cert que me dio gustavo
-    //  'local_cert' => '/etc/letsencrypt/live/monkeybootcamp.com/fullchain.pem',//certificado encontrado
+      'local_cert' => '/etc/letsencrypt/live/monkeybootcamp.com/fullchain.pem',//certificado encontrado
     //  'local_cert' => '/var/www/nomelacreoback/credenciales/server.crt',
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-      'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null), //de fabrica
+     // 'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null), //de fabrica
        //'local_pk' => '/etc/apache2/ssl/nomelacreo.com/server.key', //probando cert que me dio gustavo
-     //  'local_pk' => '/etc/letsencrypt/live/monkeybootcamp.com/privkey.pem',//certificado encontrado
+       'local_pk' => '/etc/letsencrypt/live/monkeybootcamp.com/privkey.pem',//certificado encontrado
      //  'local_pk' => '/var/www/nomelacreoback/credenciales/server.key',
 
         /*
