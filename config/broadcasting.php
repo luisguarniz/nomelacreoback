b5fded323c500fcc383efea1f55d9c0363c80174
DunkaN <lguarnizl@gmail.com>
2021-08-12 21:50:27 -0500

    primera configuracion para intentar https. hasta aca funciona correctamente http en el puerto 6002

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,18 +36,18 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-               // 'useTLS' => true,
+                'useTLS' => true,
                         'cluster' => env('PUSHER_APP_CLUSTER'),
-                //        'encrypted' => true,
-                        'host' => '127.0.0.1',
-                       //   'host' => 'www.monkeybootcamp.com', //asi funciono en planningpoker
+                        'encrypted' => true,
+                       // 'host' => '127.0.0.1',
+                          'host' => 'www.monkeybootcamp.com',
                         'port' => 6002,
-                        'scheme' => 'http'
-                     //   'scheme' => 'https',
-                     // 'curl_options' => [
-                     //   CURLOPT_SSL_VERIFYHOST => 0,
-                     //   CURLOPT_SSL_VERIFYPEER => 0,
-                   // ]
+                     //   'scheme' => 'http'
+                        'scheme' => 'https',
+                      'curl_options' => [
+                        CURLOPT_SSL_VERIFYHOST => 0,
+                        CURLOPT_SSL_VERIFYPEER => 0,
+                    ]
                 ],
             ],
 
